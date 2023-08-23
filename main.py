b18585893263200a4e39adcd4a9b36dbdc2cdd6c
yegireddihemanth <84504741+yegireddihemanth@users.noreply.github.com>
2023-08-23 12:45:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ from flask import Flask, render_template
 app = Flask(__name__)
 @app.route('/')
 def hello_world():
-    return render_template('index.html')
+    return ("<h1>mod gud</h1>")
+    # return render_template('index.html')
    
 
 # Run the app if this script is executed
