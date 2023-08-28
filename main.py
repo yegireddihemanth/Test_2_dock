85c01f42b81c3b398c7b4d3546a1b670f33f98c0
yegireddihemanth <84504741+yegireddihemanth@users.noreply.github.com>
2023-08-28 16:25:38 +0530

    resume added

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
 
 app = Flask(__name__)
 @app.route('/')
@@ -6,6 +6,10 @@ def hello_world():
     # return ("<h1>mod gud</h1>")
     return render_template('index.html')
    
+@app.route('/resume')
+def resume():
+
+    return send_from_directory('static','resume.pdf')
 
 # Run the app if this script is executed
 if __name__ == '__main__':
